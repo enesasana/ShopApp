6579e737edd70286b49d73bbc8015cf904f4d4d7
enesasana <enes_asana@outlook.com.tr>
2020-05-02 02:09:00 +0300

    Product Screens and Widget are created
    OverviewsScreen displays all products, DetailScreen shows detail of one product
    ProductItem widget created for how the only one products will be displayed
    ProductGrid widget created for the OverviewScreen

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+//import 'package:shopapp/models/product.dart';
+import 'package:shopapp/widgets/products_grid.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+      ),
+      body: ProductsGrid(),
+    );
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id,);
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          leading: IconButton(icon: Icon(Icons.favorite, color: Theme.of(context).accentColor,), onPressed: () {}),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.black87,
+          trailing: Icon(Icons.shopping_cart, color: Theme.of(context).accentColor,),
+        ),
+      ),
+    );
+  }
+}
