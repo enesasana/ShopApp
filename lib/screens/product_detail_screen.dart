9cb2fd68a358c5610f605cd657062dad9c40875c
enesasana <enes_asana@outlook.com.tr>
2020-05-03 22:06:10 +0300

    Product Item & Grid & DetailScreen updated according to Provider package
    Product Grid now has ChangeNotifier
    Product Item now has Consumer widget that allows rebuilding only on the its covered widget
    Product DetailScreen now has loadedProduct List by the help of Provider package

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/product_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 
@@ -8,8 +10,14 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
 
+    // detay için sadece bir ürünü bi kere görüyoruz, provider sınıfta herhangi
+    // bir değişiklik olursa detay sayfasında bunun her defasında rebuild
+    // olmasına ihtiyaç yok bu yüzden listen parametresi false atandı
+    final loadedProduct = Provider.of<ProductProvider>(context, listen: false).findById(
+        productId);
+
     return Scaffold(
-      appBar: AppBar(title: Text(productId),),
+      appBar: AppBar(title: Text(loadedProduct.title),),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,36 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/product.dart';
 import 'package:shopapp/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
 
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final themeOf = Theme.of(context);
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id,);
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName, arguments: product.id,);
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
-          leading: IconButton(icon: Icon(Icons.favorite, color: Theme.of(context).accentColor,), onPressed: () {}),
+          leading: Consumer<Product>(
+            // eğer child olsaydı değişmesi gereken _ yerine child parametresi olurdu
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: themeOf.accentColor,
+              ),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
+          ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           backgroundColor: Colors.black87,
-          trailing: Icon(Icons.shopping_cart, color: Theme.of(context).accentColor,),
+          trailing: Icon(
+            Icons.shopping_cart,
+            color: themeOf.accentColor,
+          ),
         ),
       ),
     );
