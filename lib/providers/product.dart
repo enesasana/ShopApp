4e6ad4410b3b6aeaa66685b6dad00ea3c4dcc23b
enesasana <enes_asana@outlook.com.tr>
2020-05-12 23:28:53 +0300

    Updating Favorite Status with Optimistic Updating

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 class Product with ChangeNotifier {
   final String id;
   final String title;
@@ -17,9 +19,27 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavoriteValue(bool newValue){
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = 'https://shop-app-467f5.firebaseio.com/products/$id.json';
+    try{
+      final response = await http.patch(url, body: json.encode({
+        'isFavorite': isFavorite,
+      }));
+      if(response.statusCode >= 400) {
+        _setFavoriteValue(oldStatus);
+      }
+    } catch (error) {
+      // Rolling back
+      _setFavoriteValue(oldStatus);
+    }
   }
 
 }
